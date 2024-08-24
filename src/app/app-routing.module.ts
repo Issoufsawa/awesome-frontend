bee607000ae3b8ad2c0da958d9994b57355c06f8
moussakante12 <moussa.kante@yoomi-solution.com>
2024-08-24 20:17:07 +0000

    state-management

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SocialMediaModule } from './social-media/social-media.module';
 import { ComplexFormModule } from './complex-form/complex-form.module';
+import { ReactiveStateModule } from './reactive-state/reactive-state.module';
 
 const routes: Routes = [
   {path: 'social-media',loadChildren:()=> import('./social-media/social-media.module').then(m=>SocialMediaModule)},
   {path: 'complex-form',loadChildren:()=> import('./complex-form/complex-form.module').then(m=>ComplexFormModule)},
+  {path: 'reactive-state',loadChildren:()=> import('./reactive-state/reactive-state.module').then(m=>ReactiveStateModule)},
   {path: '**', redirectTo:'social-media'}
 ];
 
